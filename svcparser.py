9e7b4fc8f97ea183d17de283b03b47c56d75c43c
drbngd <55194087+drbngd@users.noreply.github.com>
2023-05-13 03:06:21 -0500

    Update svcparser.py

--- svcparser.py
+++ svcparser.py
@@ -1,160 +1,62 @@
-# SVC Parser - Synthesized Verilog Circuit
-import re
+# =============================================================================
+# author:       Dhruv Raj Bangad (bangaddj@mail.uc.edu)
+# purpose:      To parse the trojan porbability info into a pandas dataframe
+# usage:        from trojanprobability import getTrojanProbability 
+#               after importing, use it as function
+# return value: pandas dataframe
+# =============================================================================
 
-# FUNCTION: searched for a pattern in the given string
-def checkPattern(pattern_list, str_list):
-    for pattern in pattern_list:
-        for str in str_list:
-            if pattern in str:
-                return str
-    return None
+import os, sys, re, math, pandas as pd
+from trojanparser import TrojanParser
 
-class CircuitParser:
-    def __init__(self, filename):
-        self.module_name = ''
-        self.module_line = ''
-        self.inputs = []
-        self.outputs = []
-        self.wires = []
-        self.registers = []
-        self.clock = ''
-        self.reset = ''
 
-        # Define regular expressions to match module name, inputs, outputs, wires, and registers
-        # for explaination: https://regex101.com/r/0CXHON/1
-        module_re = r"(^([^\/\/].*)?^\s*module\s+(\w+)\s*\(((\s*.*\w+,*\s*)+\s*)\);)"
-        input_re = r"^([^\/\/].*)?^\s*(input\s*((\s*.*\w+,*\s*)+\s*);)"
-        output_re = r"^([^\/\/].*)?^\s*(output\s*((\s*.*\w+,*\s*)+\s*);)"
-        wire_re = r"^([^\/\/].*)?^\s*(wire\s*((\s*.*\w+,*\s*)+\s*);)"
-        register_re = r"^([^\/\/].*)?^\s*((\w+)\s+(.+)\s*(\((\s*.\w+\s*\(\s*.+\s*\),*\s*)+\s*\)));"
-     
-        file = open(filename, 'r').read()
 
-        # Find the module name
-        matches = re.finditer(module_re, file, re.MULTILINE)
-        for matchNum, match in enumerate(matches, start=1):
-            self.module_name = match.group(3)
-            self.module_line = match.group(1)
-            print("Module name:", self.module_name)
-            print(self.module_line)
+def getTrojanProbability(trojan_file):
 
-        # Find the inputs
-        matches = re.finditer(input_re, file, re.MULTILINE)
-        for matchNum, match in enumerate(matches, start=1):
-            self.inputs.append((re.sub(r'\s+', '', match.group(3)).split(",")))
-        print("Inputs:", self.inputs)
+    # generating the probability file
+    x = trojan_file.strip('.v')
+    prob_fp = f'temp_prob.txt'
+    cmd = f'./prob junk.txt {trojan_file} > {prob_fp}'
+    os.system(cmd)
 
-        # Finding clock and reset in inputs
-        clock_search_text = ['clk', 'CLK', 'clock', 'CLOCK', 'Clock']
-        reset_search_text = ['reset', 'RESET', 'Reset']
+    # initializing the pandas dataframe
+    df = pd.DataFrame(columns=['node_name', 'p_low', 'p_high', 'cc0', 'cc1', 'c0', 'scope_testability'], index=[])
 
-        # checking for clock & reset in inputs
-        for input_list in self.inputs:
-            self.clock = checkPattern(clock_search_text, input_list)
-            self.reset = checkPattern(reset_search_text, input_list)
-            if self.clock and self.reset:
-                print(f'CLOCK: {self.clock} \nRESET: {self.reset}')                
-                break        
-
-        # Find the outputs
-        matches = re.finditer(output_re, file, re.MULTILINE)
-        for matchNum, match in enumerate(matches, start=1):
-            self.outputs.append((re.sub(r'\s+', '', match.group(3)).split(",")))
-        print("outputs:", self.outputs)
-
-        # Find the wires
-        matches = re.finditer(wire_re, file, re.MULTILINE)
-        for matchNum, match in enumerate(matches, start=1):
-            self.wires.append((re.sub(r'\s+', '', match.group(3)).split(",")))
-        print("wires:", self.wires)
-
-        # Find the registers
-        matches = re.finditer(register_re, file, re.MULTILINE)
-        for matchNum, match in enumerate(matches, start=1):
-            register_type = match.group(3)
-            register_name = match.group(4)
-            register_args = re.sub(r'\s+', '', match.group(5))
-            self.registers.append((register_type, register_name, register_args))
-        print("Registers:", self.registers)
-
-    def getModuleName(self):
-        return self.module_name
-
-    def getModuleLine(self):
-        return self.module_line
-
-    def getInputs(self):
-        return self.inputs
-
-    def getOutputs(self):
-        return self.outputs
+    # parsing the probability file to get the pandas dataframe
+    fp = open(prob_fp, 'r')
+    lines = fp.readlines()
     
-    def getWires(self):
-        return self.wires
+    prob_pattern = r'(\w+)\s+\[(\d+(\.\d+)?)\s+(\d+(\.\d+)?)\]'
+    scoap_pattern = r'Gate\s*:\s*(\w+)\s*CC0\s*:\s*(\d+)\s*CC1\s*:\s*(\d+)\s*CO\s*:\s*(\d+)'
 
-    def getRegisters(self):
-        return self.registers
+    for line in lines:
 
-    def getClockAndReset(self):
-        return self.clock, self.reset
+        # since Probability is displayed in the beginning of the generated file
+        # hence, we first find probability values and add them to the data frame
+        if 'Probabaility :=' in line:
+            match = re.search(prob_pattern, line)
+            if match:
+                node = match.group(1)                    # gets node name through regex
+                probs_0 = float(match.group(2))                                # gets probability through regex
+                probs_1 = float(match.group(4))
+                new_row = {'node_name': node, 'p_low': probs_0, 'p_high': probs_1} # new row to add to dataframe
+                df.loc[len(df.index)] = new_row                                         # appends new row to dataframe end
+        
+        # scope values are displayed after the probability values,
+        # so we update the particular node row with the scope values
+        elif 'Gate :' in line:
+            node = re.findall(r'Gate :  (.*?)  CC', line)[0]                               # gets node name through regex              
+            scope = re.findall(r'\s(\d+)', line)                                           # gets CC0, CC1, C0 values 
+            #scope_test = math.dist((int(scope[0]), int(scope[1]), int(scope[2])), (0,0,0)) # computes the scope testability
+            scope_test = math.sqrt(int(scope[0])**2 + int(scope[1])**2 + int(scope[2])**2) # computes the scope for python 3.6 and less
+            scope.append(scope_test)                                                       # append it to the scope list
+            insert_loc = df.index[df['node_name']== node].tolist()[0]                      # finds insert location
+            df.loc[insert_loc, ['cc0', 'cc1', 'c0', 'scope_testability']] = scope          # updates df with new values for the node
+    
+    fp.close()
+#    os.system(f'rm -rf {prob_fp}')     
+    return df
 
-    def setModuleName(self, module_name):
-        self.module_name = module_name
-
-    def setModuleLine(self, module_line):
-        self.module_line = module_line
-
-    def setInputs(self, inputs):
-        self.inputs = inputs
-
-    def setOutputs(self, outputs):
-        self.outputs = outputs
-
-    def setWires(self, wires):
-        self.wires = wires
-
-    def setRegisters(self, registers):
-        self.registers = registers
-
-    def setClockAndReset(self, clock, reset):
-        self.clock = clock
-        self.reset = reset
-
-    # TODO: implement this function
-    def makeCircuit(self):
-        data = ''
-        # add module name
-        #data += 'module {name}({inouts});\n'.format(name = self.module_name, inouts = ', '.join(self.inputs+self.outputs))
-        print(self.module_line)
-        data += self.module_line + '\n'
-
-        # add inputs
-        for input_list in self.inputs:
-            data += '\tinput {inp};\n'.format(inp = ', '.join(input_list))
-
-        # add outputs
-        for output_list in self.outputs:
-            data += '\toutput {outp};\n'.format(outp = ', '.join(output_list))
-
-        # add wires
-        for wire_list in self.wires:
-            data += '\twire {wi};\n'.format(wi = ', '.join(wire_list))
-
-        # add registers
-        for i,reg in enumerate(self.registers):
-#            if reg[1] in self.wires:
-#                new_name = reg[1]           
-            if re.match(r'U\d+', reg[1]):
-                new_name = f'U{i}'
-            elif reg[1] == '':
-                new_name = ''
-            else:
-                new_name = reg[1]
-            data += f'\t{reg[0]} {new_name} {reg[2]};\n'
-
-        # add endmodule
-        data += '\nendmodule'
-
-        result_filename = f'trjIN{self.module_name}.v'
-        with open(result_filename, "w") as file:
-            file.write(data)
+# for testing
+d = getTrojanProbability(str(sys.argv[1]))
+print(d)
