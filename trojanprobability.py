d927ec22acbd9282a312a328d3417618f973fecb
drbngd <55194087+drbngd@users.noreply.github.com>
2023-05-15 01:39:11 -0500

    Update trojanprobability.py
    prev version had issue with capturing all probability values, specially values containing 'e10'. The new version does that.

--- trojanprobability.py
+++ trojanprobability.py
@@ -9,7 +9,6 @@
 import os, sys, re, math, pandas as pd
 
 
-
 def getTrojanProbability(trojan_file):
 
     # generating the probability file
@@ -25,8 +24,12 @@ def getTrojanProbability(trojan_file):
     fp = open(prob_fp, 'r')
     lines = fp.readlines()
     
-    prob_pattern = r'(\w+)\s+\[(\d+(\.\d+)?)\s+(\d+(\.\d+)?)\]'
+    # prob_pattern = r'(\w+)\s+\[(\d+(\.\d+)?)\s+(\d+(\.\d+)?)\]'
+    '''prob pattern made by me'''
+    prob_pattern = r'Probabaility\s:=\s\s(\S*)\s\[(\S*)\s(\S*)\]'
+
     scoap_pattern = r'Gate\s*:\s*(\w+)\s*CC0\s*:\s*(\d+)\s*CC1\s*:\s*(\d+)\s*CO\s*:\s*(\d+)'
+    scoap_pattern = r'Gate\s:\s\s(\S+)\s\sCC0\s:\s\s(\S+)\s\sCC1\s:\s\s(\S+)\s\sCO\s:\s\s(\S+)'
 
     for line in lines:
 
@@ -37,19 +40,40 @@ def getTrojanProbability(trojan_file):
             if match:
                 node = match.group(1)                    # gets node name through regex
                 probs_0 = float(match.group(2))                                # gets probability through regex
-                probs_1 = float(match.group(4))
+                probs_1 = float(match.group(3))
                 new_row = {'node_name': node, 'p_low': probs_0, 'p_high': probs_1} # new row to add to dataframe
                 df.loc[len(df.index)] = new_row                                         # appends new row to dataframe end
         
         # scope values are displayed after the probability values,
         # so we update the particular node row with the scope values
         elif 'Gate :' in line:
-            node = re.findall(r'Gate :  (.*?)  CC', line)[0]                               # gets node name through regex              
-            scope = re.findall(r'\s(\d+)', line)                                           # gets CC0, CC1, C0 values 
+            match = re.search(scoap_pattern, line)
+            if match:
+                node = match.group(1)
+                scope = [int(match.group(2)), int(match.group(3)), int(match.group(4))]
+                
+                '''prev version'''
+                # node = re.findall(r'Gate :  (.*?)  CC', line)[0]                               # gets node name through regex              
+                # scope = re.findall(r'\s(\d+)', line)                                           # gets CC0, CC1, C0 values 
+            
+            
             #scope_test = math.dist((int(scope[0]), int(scope[1]), int(scope[2])), (0,0,0)) # computes the scope testability
             scope_test = math.sqrt(int(scope[0])**2 + int(scope[1])**2 + int(scope[2])**2) # computes the scope for python 3.6 and less
             scope.append(scope_test)                                                       # append it to the scope list
-            insert_loc = df.index[df['node_name']== node].tolist()[0]                      # finds insert location
+            
+            #insert_loc = df.index[df['node_name']== node].tolist()[0]                      # finds insert location
+            
+            '''trying to catch error'''
+            insert_locs = df.index[df['node_name'] == node].tolist()
+            if insert_locs:
+                insert_loc = insert_locs[0]
+                # Proceed with further operations using `insert_loc`
+            else:
+                # Handle the case when no matching rows are found
+                print(f'{trojan_file} had error with finding following node: {node}')
+
+            
+            
             df.loc[insert_loc, ['cc0', 'cc1', 'c0', 'scope_testability']] = scope          # updates df with new values for the node
     
     fp.close()
